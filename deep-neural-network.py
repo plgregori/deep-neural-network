15135f1c15b5dcc6d9f1d2e4719cae4668889271
paolo.gregori <plgregori@gmail.com>
2023-03-15 17:31:50 +0100

    major changes

--- deep-neural-network.py
+++ deep-neural-network.py
@@ -5,17 +5,23 @@ from testCases import *
 from dnn_utils import sigmoid, sigmoid_backward, relu, relu_backward
 from public_tests import *
 
+import time
+import scipy
+from PIL import Image
+from scipy import ndimage
+from dnn_app_utils_v3 import *
+
 # We start by initializing the parameters W and b. W to random matrixes, b to zero vectors.
 
 def initialize_parameters_deep(layer_dims):
     
-    np.random.seed(3)
+    np.random.seed(1)
     parameters = {}
     L = len(layer_dims) # number of layers in the network
 
     for l in range(1, L):
         
-        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) / np.sqrt(layer_dims[l-1]) # Note that here we are dividing by its square root instead of multiplying by 0.01. This is to aling with dnn_app_utils_v3.py
         parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
         
         assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
@@ -77,7 +83,7 @@ def compute_cost(AL, Y):
         
     m = Y.shape[1]
     cost = -(1/m)* np.sum(np.multiply(np.log(AL),Y) + np.multiply(np.log(1-AL),1-Y)) 
-    cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
+    cost = np.array(np.squeeze(cost))     # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
 
     return cost
 
@@ -152,3 +158,180 @@ def update_parameters(params, grads, learning_rate):
 
     return parameters
 
+# The model is now ready to be applied. We first load the data and print one example
+
+train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+index = 10
+plt.imshow(train_x_orig[index])
+plt.show()
+print ("y = " + str(train_y[0,index]) + ". It's a " + classes[train_y[0,index]].decode("utf-8") +  " picture.")
+
+# We familiarize ourselves with the data
+
+m_train = train_x_orig.shape[0]
+num_px = train_x_orig.shape[1]
+m_test = test_x_orig.shape[0]
+
+print ("Number of training examples: " + str(m_train))
+print ("Number of testing examples: " + str(m_test))
+print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print ("train_x_orig shape: " + str(train_x_orig.shape))
+print ("train_y shape: " + str(train_y.shape))
+print ("test_x_orig shape: " + str(test_x_orig.shape))
+print ("test_y shape: " + str(test_y.shape))
+
+# We now reshape and standardize the pictures
+
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   # The "-1" makes reshape flatten the remaining dimensions
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+
+train_x = train_x_flatten/255.
+test_x = test_x_flatten/255.
+
+print ("train_x's shape: " + str(train_x.shape))
+print ("test_x's shape: " + str(test_x.shape))
+
+# We now check the model in the simple 2-layer case
+
+n_x = 12288
+n_h = 7
+n_y = 1
+layers_dims = (n_x, n_h, n_y)
+learning_rate = 0.0075
+
+# For the 2-layer example, we introduce an initialization function (note that this is not really necessary, we do it only for testing purposes)
+
+def initialize_parameters(n_x, n_h, n_y):
+   
+    np.random.seed(1)
+    
+    W1 = np.random.randn(n_h,n_x) * 0.01
+    b1 = np.zeros((n_h,1))
+    W2 = np.random.randn(n_y,n_h) * 0.01
+    b2 = np.zeros((n_y,1))
+    
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+# We now define the 2-layer model function
+
+def two_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):
+    
+    np.random.seed(1)
+    grads = {}
+    costs = []                              # to keep track of the cost
+    m = X.shape[1]                          # number of examples
+    (n_x, n_h, n_y) = layers_dims
+    
+    parameters = initialize_parameters(n_x, n_h, n_y)
+
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    for i in range(0, num_iterations):
+
+        A1, cache1 = linear_activation_forward(X, W1, b1, activation = "relu")
+        A2, cache2 = linear_activation_forward(A1, W2, b2, activation = "sigmoid")
+        
+        cost = compute_cost(A2, Y)
+        
+        dA2 = - (np.divide(Y, A2) - np.divide(1 - Y, 1 - A2))
+        
+        dA1, dW2, db2 = linear_activation_backward(dA2, cache2, activation = "sigmoid")
+        dA0, dW1, db1 = linear_activation_backward(dA1, cache1, activation = "relu")
+        
+        grads['dW1'] = dW1
+        grads['db1'] = db1
+        grads['dW2'] = dW2
+        grads['db2'] = db2
+        
+        parameters = update_parameters(parameters, grads, learning_rate)
+        
+        W1 = parameters["W1"]
+        b1 = parameters["b1"]
+        W2 = parameters["W2"]
+        b2 = parameters["b2"]
+        
+        if print_cost and i % 100 == 0 or i == num_iterations - 1:
+            print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
+        if i % 100 == 0 or i == num_iterations:
+            costs.append(cost)
+
+    return parameters, costs
+
+# We introduce a function that plots the costs
+
+def plot_costs(costs, learning_rate=0.0075):
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+
+# Now we test the 2-layer model with 2500 iterations. We plot the costs, the accuracy for the training set, and the accuray for the test set
+
+parameters, costs = two_layer_model(train_x, train_y, layers_dims = (n_x, n_h, n_y), num_iterations = 2500, print_cost=True)
+plot_costs(costs, learning_rate)
+predictions_train = predict(train_x, train_y, parameters)   # The predict function is defined in dnn_app_utils_v3.py
+predictions_test = predict(test_x, test_y, parameters)
+
+# We are now ready to define the L-layer model
+
+layers_dims = [12288, 20, 7, 5, 1] # 4-layer model
+
+def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):
+    
+    np.random.seed(1)
+    costs = []
+    
+    parameters = initialize_parameters_deep(layers_dims)
+    
+    for i in range(0, num_iterations):
+
+        AL, caches = L_model_forward(X, parameters)
+        
+        cost = compute_cost(AL, Y)
+       
+        grads = L_model_backward(AL, Y, caches)
+        
+        parameters = update_parameters(parameters, grads, learning_rate)
+        
+        if print_cost and i % 100 == 0 or i == num_iterations - 1:
+            print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
+        if i % 100 == 0 or i == num_iterations:
+            costs.append(cost)
+    
+    return parameters, costs
+
+# We can now test the 4-layer model
+
+parameters, costs = L_layer_model(train_x, train_y, layers_dims, num_iterations = 2500, print_cost = True)
+plot_costs(costs, learning_rate)
+pred_train = predict(train_x, train_y, parameters)
+pred_test = predict(test_x, test_y, parameters)
+
+print_mislabeled_images(classes, test_x, test_y, pred_test)
+plt.show()
+
+# We can now test the model on an image of our choosing
+
+my_image = "my_image2.jpg"
+my_label_y = [1]
+
+fname = "images/" + my_image
+image = np.array(Image.open(fname).resize((num_px, num_px)))
+plt.imshow(image)
+image = image / 255.
+image = image.reshape((1, num_px * num_px * 3)).T
+
+my_predicted_image = predict(image, my_label_y, parameters)
+
+print ("y = " + str(np.squeeze(my_predicted_image)) + ", your L-layer model predicts a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
+plt.show()
